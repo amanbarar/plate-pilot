523a048d8e5be030a802d1a8946eb592bfe936be
Aman Barar <barar.amanjeet.aman@gmail.com>
2023-12-01 01:54:42 +0530

    fix: footer content

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return ( 
-        <footer className="main-footer">
-            <strong>Copyright © 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
-            All rights reserved.
-            <div className="float-right d-none d-sm-inline-block">
-                <b>Version</b> 3.0.5
-            </div>
-        </footer>
-     );
-}
- 
+  return <footer className="main-footer"></footer>;
+};
+
 export default Footer;
